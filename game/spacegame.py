7e36841856d7000e3d891cfed45b7f4bff773cee
MbIUIb <kirill.chezlov@gmail.com>
2022-08-14 01:30:27 +0300

    add mouse control

--- game/spacegame.py
+++ game/spacegame.py
@@ -17,6 +17,10 @@ pygame.time.set_timer(pygame.USEREVENT, 70)
 stars = pygame.sprite.Group()
 hero = Starship(screen_width//2, screen_heidth//2)
 
+mouse_control = True
+mouse_visible = True
+pygame.mouse.set_visible(mouse_visible)
+
 game = True
 while game:
     for event in pygame.event.get():
@@ -25,24 +29,29 @@ while game:
         elif event.type == pygame.USEREVENT:
             create_stars(stars)
 
-    # it is necessary to process if-s in separate constructions
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        hero.left_movement()
-    if keys[pygame.K_RIGHT]:
-        hero.right_movement()
-    if keys[pygame.K_UP]:
-        hero.up_movement()
-    if keys[pygame.K_DOWN]:
-        hero.down_movement()
+    if mouse_control:
+        mouse_pos = pygame.mouse.get_pos()
+        hero.follow_mouse(mouse_pos)
+    else:
+        # it is necessary to process 'if'-s in separate constructions
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+            hero.left_movement()
+        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+            hero.right_movement()
+        if keys[pygame.K_UP] or keys[pygame.K_w]:
+            hero.up_movement()
+        if keys[pygame.K_DOWN] or keys[pygame.K_s]:
+            hero.down_movement()
 
+    # firstly drawing
     screen.fill(SPACE)
 
-
+    # secondly drawing
     stars.draw(screen)
     stars.update(screen_heidth)
 
-
+    # third drawing
     screen.blit(hero.image, hero.rect)
 
     pygame.display.update()
