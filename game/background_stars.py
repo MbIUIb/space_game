1a69468a81328aa8752f6d9ff73b93d98c98e5fd
MbIUIb <kirill.chezlov@gmail.com>
2022-08-14 01:28:18 +0300

    draw area fix

--- game/background_stars.py
+++ game/background_stars.py
@@ -14,7 +14,7 @@ class Star(Sprite):
         self.add(group)
 
     def update(self, *args):
-        if self.rect.y < args[0] - 20:
+        if self.rect.y < args[0]:
             self.rect.y += self.speed
         else:
             self.kill()
