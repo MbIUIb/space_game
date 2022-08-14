5efdaf76ed7f305cd4f6885ab78c561eee1d012d
MbIUIb <kirill.chezlov@gmail.com>
2022-08-15 00:19:58 +0300

    code update, new user events, refactoring

--- game/spacegame.py
+++ game/spacegame.py
@@ -1,45 +1,54 @@
 import pygame
 
 from config import *
-from starship import Starship
+from starships import HeroStarShip, EnemyStarShip
 from background_stars import create_stars
-
+from tools import load_image
 
 pygame.init()
 
 screen = pygame.display.set_mode((screen_width, screen_heidth))
 
-icon = pygame.image.load('assets/icon.png')
+icon = load_image(icon)
 pygame.display.set_icon(icon)
-pygame.display.set_caption('Space Game')
+pygame.display.set_caption(game_name)
 
 clock = pygame.time.Clock()
-# time between creation of stars
-pygame.time.set_timer(pygame.USEREVENT, 70)
 
+# user events
+FLYING_STAR = pygame.event.custom_type()
+pygame.time.set_timer(FLYING_STAR, 70)
+SHOOTING_HERO = pygame.event.custom_type()
+pygame.time.set_timer(SHOOTING_HERO, 150)
+
+# sprite groups
 stars = pygame.sprite.Group()
-hero = Starship(screen_width//2, screen_heidth//2)
+hero_bullets = pygame.sprite.Group()
+enemy_bullets = pygame.sprite.Group()
 
-mouse_control = True
-mouse_visible = True
+hero = HeroStarShip(screen_width // 2, screen_heidth - 100)
+enemy = EnemyStarShip(screen_width // 2, 100)
+
 pygame.mouse.set_visible(mouse_visible)
 
-game = True
+running_game = True
 
-
-while game:
+while running_game:
+    keys = pygame.key.get_pressed()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            game = False
-        elif event.type == pygame.USEREVENT:
+            running_game = False
+        elif event.type == FLYING_STAR:
             create_stars(stars)
+        elif event.type == SHOOTING_HERO:
+            if keys[pygame.K_SPACE]:
+                hero.shoot(hero_bullets)
 
     if mouse_control:
         mouse_pos = pygame.mouse.get_pos()
         hero.follow_mouse(mouse_pos)
     else:
         # it is necessary to process 'if'-s in separate constructions
-        keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT] or keys[pygame.K_a]:
             hero.left_movement()
         if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
@@ -56,8 +65,12 @@ while game:
     stars.draw(screen)
     stars.update(screen_heidth)
 
+    hero_bullets.draw(screen)
+    hero_bullets.update(-1)
+
     # third drawing
     screen.blit(hero.image, hero.rect)
+    screen.blit(enemy.image, enemy.rect)
 
     pygame.display.update()
     clock.tick(FPS)
