b94a7d86efb4b9826aa1d0ae5c8b0bbebe6c449c
MbIUIb <kirill.chezlov@gmail.com>
2022-08-15 00:14:17 +0300

    create Bullet()

---
+++ game/bullet.py
@@ -0,0 +1,23 @@
+import pygame
+from pygame.sprite import Sprite
+
+from config import screen_width, screen_heidth
+from tools import load_image
+
+
+class Bullet(Sprite):
+    def __init__(self, x, y, group, damage=10, speed=15):
+        super().__init__()
+        self.x = x
+        self.y = y
+        self.speed = speed
+        self.damage = damage
+        self.image = load_image('bullet3x10.png')
+        self.rect = self.image.get_rect(center=(self.x, self.y))
+        self.add(group)
+
+    def update(self, updirection=1):
+        if 0 < self.rect.y < screen_heidth:
+            self.rect.y += self.speed * updirection
+        else:
+            self.kill()
