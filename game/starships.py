35caf86a39915cb1a3fe352bf4befc5b1cbcc792
MbIUIb <kirill.chezlov@gmail.com>
2022-08-15 21:07:20 +0300

    rearchitecturing, fixed incorrectly set position of coordinates and update method, new shooting system

--- game/starships.py
+++ game/starships.py
@@ -1,81 +1,98 @@
+from time import time
 import pygame
 from pygame.sprite import Sprite
 from pygame.math import Vector2 as V2
 
-from config import screen_width, screen_heidth
+from config import screen_width, screen_heigth
 from tools import load_image, unscale_image
 from bullet import Bullet
 
 
 class StarShip(Sprite):
-    def __init__(self, x, y):
+    def __init__(self, x, y, image, group):
         super().__init__()
-        self.x = x
-        self.y = y
+        self.vel = V2()
+        self.image = load_image(image)
+        self.rect = self.image.get_rect(center=(x, y))
+        self.health = 100
         self.speed = 7
+        self.speed4mouse = None
 
-    def coords_update(self):
-        self.rect = self.image.get_rect(center=(self.x, self.y))
+        # bullet specification
+        self.bullet_speed = None
+        self.bullet_damage = None
+        self.fire_pace = 0.15
+        self.fire_flag = 0
+
+        self.add(group)
 
     def left_movement(self):
-        self.x -= self.speed
-        if self.x < 0:
-            self.x = 0
-        self.coords_update()
+        self.rect.centerx -= self.speed
+        if self.rect.centerx < 0:
+            self.rect.centerx = 0
 
     def right_movement(self):
-        self.x += self.speed
-        if self.x > screen_width:
-            self.x = screen_width
-        self.coords_update()
+        self.rect.centerx += self.speed
+        if self.rect.centerx > screen_width:
+            self.rect.centerx = screen_width
 
     def up_movement(self):
-        self.y -= self.speed
-        if self.y < 0:
-            self.y = 0
-        self.coords_update()
+        self.rect.centery -= self.speed
+        if self.rect.centery < 0:
+            self.rect.centery = 0
 
     def down_movement(self):
-        self.y += self.speed
-        if self.y > screen_heidth:
-            self.y = screen_heidth
-        self.coords_update()
-
-
-class HeroStarShip(StarShip):
-    def __init__(self, x, y):
-        super().__init__(x, y)
-        self.vel = V2()
-        self.image = load_image('icon.png')
-        self.rect = self.image.get_rect(center=(self.x, self.y))
-        self.health = 100
-        self.bullet_damage = 10
-        self.bullet_speed = 15
-        self.speed4mouse = 12
+        self.rect.centery += self.speed
+        if self.rect.centery > screen_heigth:
+            self.rect.centery = screen_heigth
 
     def follow_mouse(self, mouse_pos):
+        """experimental function"""
         self.vel.update(mouse_pos[0] - self.x, mouse_pos[1] - self.y)
 
         if self.vel.length():
             self.vel.scale_to_length(self.speed4mouse)
 
-        if abs(self.x - mouse_pos[0]) >= self.speed // 2:
-            self.x += self.vel.x
-        if abs(self.y - mouse_pos[1]) >= self.speed // 2:
-            self.y += self.vel.y
-
-        self.coords_update()
+        if abs(self.rect.centerx - mouse_pos[0]) >= self.speed // 2:
+            self.rect.centerx += self.vel.x
+        if abs(self.rect.centery - mouse_pos[1]) >= self.speed // 2:
+            self.rect.centery += self.vel.y
 
     def shoot(self, group):
-        return Bullet(self.x, self.y-45, group, self.bullet_damage, self.bullet_speed)
+        fire_time = time()
+        if fire_time - self.fire_flag > self.fire_pace:
+            self.fire_flag = fire_time
+            return Bullet(self.rect.centerx, self.rect.centery-45, group, self.bullet_damage, self.bullet_speed)
 
 
+class HeroStarShip(StarShip):
+    def __init__(self, x, y, image, group):
+        super().__init__(x, y, image, group)
+        self.speed4mouse = 12
+
+        self.bullet_speed = 15
+        self.bullet_damage = 10
+
+
+x_direction = 1
 class EnemyStarShip(StarShip):
-    def __init__(self, x, y, filename='enemy.png'):
-        super().__init__(x, y)
-        self.image = load_image(filename)
-        self.rect = self.image.get_rect(center=(self.x, self.y))
-        self.health = 100
-        self.bullet_damage = 5
+    def __init__(self, x, y, image, group):
+        super().__init__(x, y, image, group)
+
         self.bullet_speed = 7
-        self.speed = 5
+        self.bullet_damage = 5
+        self.fire_pace = 0.5
+
+    def update(self):
+        global x_direction
+        if self.rect.centerx < 50:
+            x_direction = 1
+        if self.rect.centerx > 750:
+            x_direction = -1
+        self.rect.centerx += self.speed * x_direction
+
+    def shoot(self, group):
+        fire_time = time()
+        if fire_time - self.fire_flag > self.fire_pace:
+            self.fire_flag = fire_time
+            return Bullet(self.rect.centerx, self.rect.centery+45, group, self.bullet_damage, self.bullet_speed)
