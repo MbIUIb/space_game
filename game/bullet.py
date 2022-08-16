5ce2c908f732d5787faaf7f676f6badafef3b894
MbIUIb <kirill.chezlov@gmail.com>
2022-08-16 14:21:59 +0300

    add full support for vector movement

--- game/bullet.py
+++ game/bullet.py
@@ -9,16 +9,18 @@ from tools import load_image
 class Bullet(Sprite):
     def __init__(self, x, y, group, speed: V2, damage=10):
         super().__init__()
-        self.x = x
-        self.y = y
         self.speed = speed
         self.damage = damage
         self.image = load_image('bullet3x10.png')
-        self.rect = self.image.get_rect(center=(self.x, self.y))
+        self.rect = self.image.get_rect(center=(x, y))
         self.add(group)
 
     def update(self):
         if 0 < self.rect.centery < screen_heigth:
             self.rect.centery += self.speed.y
+        else:
+            self.kill()
+        if 0 < self.rect.centerx < screen_heigth:
+            self.rect.centerx += self.speed.x
         else:
             self.kill()
