01bb198fd93e86426e146ede06dc19183bfa6dfd
MbIUIb <kirill.chezlov@gmail.com>
2022-08-16 22:23:15 +0300

    refactor

--- game/bullet.py
+++ game/bullet.py
@@ -2,17 +2,17 @@ import pygame
 from pygame.sprite import Sprite
 from pygame.math import Vector2 as V2
 
-from config import screen_width, screen_heigth
+from config import screen_width, screen_heigth, heroes, enemies
 from tools import load_image
 
 
 class Bullet(Sprite):
     def __init__(self, x, y, group, speed: V2, damage=10):
         super().__init__()
-        self.speed = speed
-        self.damage = damage
         self.image = load_image('bullet3x10.png')
         self.rect = self.image.get_rect(center=(x, y))
+        self.speed = speed
+        self.damage = damage
         self.add(group)
 
     def update(self):
