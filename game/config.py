2ac2df7e588f2f1db1a722e0bece09cb261e000e
Krai53n <greasha46@gmail.com>
2022-08-18 11:04:54 +0300

    update branch

--- game/config.py
+++ game/config.py
@@ -1,4 +1,8 @@
-import pygame
+from dataclasses import dataclass
+
+import pygame as pg
+
+
 # colors
 SPACE = (3, 4, 49)
 OUTLINE = (250, 65, 65)
@@ -8,28 +12,29 @@ SCORE = (206, 137, 255)
 # STAR_lvl2 = (198, 198, 206)
 # STAR_lvl3 = (109, 102, 114)
 
-# fonts
-arkhip = 'assets/fonts/Arkhip.ttf'
-impact = 'assets/fonts/Impact.ttf'
-wiguru = 'assets/fonts/WiGuru.ttf'
+@dataclass
+class FontNames:
+    arkhip = 'assets/fonts/Arkhip.ttf'
+    impact = 'assets/fonts/Impact.ttf'
+    wiguru = 'assets/fonts/WiGuru.ttf'
 
 # screen
 icon = 'icon.png'
 game_name = 'Space Game'
 screen_width = 800
 screen_heigth = 800
-screen = pygame.display.set_mode((screen_width, screen_heigth))
+screen = pg.display.set_mode((screen_width, screen_heigth))
 
 # initial mouse control settings
 mouse_control = False
 mouse_visible = False
 
 # sprite groups
-stars = pygame.sprite.Group()
-heroes = pygame.sprite.Group()
-hero_bullets = pygame.sprite.Group()
-enemies = pygame.sprite.Group()
-enemy_bullets = pygame.sprite.Group()
+stars = pg.sprite.Group()
+heroes = pg.sprite.Group()
+hero_bullets = pg.sprite.Group()
+enemies = pg.sprite.Group()
+enemy_bullets = pg.sprite.Group()
 
 FPS = 60
 
